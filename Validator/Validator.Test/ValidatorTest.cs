2de9fbf8933dfdfc1bdcc6f4531b5f49bf99b781
samuel <samuel.lindblom@hotmail.com>
2022-08-01 11:03:39 +0200

    wip

--- Validator/Validator.Test/ValidatorTest.cs
+++ Validator/Validator.Test/ValidatorTest.cs
@@ -7,21 +7,6 @@ using Validator.Helpers;
 
 public class ValidatorTest
 {
-    // with one predicate
-    // with one predicate and message
-    // with one predicate and one condition
-    // with one predicate and one condition and message
-
-    // with multiple chained predicates
-    // with multiple chained predicates and messages
-    // with multiple chained predicates and one condition before
-    // with multiple chained predicates and one condition in middle
-
-    // with multiple non chained predicates
-
-    // with nested validator
-    // with nested validator and one condition
-
     public abstract class Common<TValidator> where TValidator : IValidator<TestModel>, new()
     {
         public static Result Validate(TestModel model)
@@ -42,7 +27,7 @@ public class ValidatorTest
             [Test]
             public void should_not_have_any_failures_when_predicate_pass()
             {
-                Assert.That(Validate("asdf").Failures.Any(), Is.False);
+                Assert.That(Validate("asdf").Failures, Is.Empty);
             }
 
             [Test]
@@ -54,7 +39,7 @@ public class ValidatorTest
             [Test]
             public void should_have_one_failure_when_predicate_fail()
             {
-                Assert.That(Validate("").Failures.Count(), Is.EqualTo(1));
+                Assert.That(Validate("").Failures, Has.Count.EqualTo(1));
             }
 
             public class TestValidator : Validator<TestModel>
@@ -71,7 +56,7 @@ public class ValidatorTest
             [Test]
             public void should_not_have_any_failures_when_predicate_pass()
             {
-                Assert.That(Validate("asdf").Failures.Any(), Is.False);
+                Assert.That(Validate("asdf").Failures, Is.Empty);
             }
 
             [Test]
@@ -83,7 +68,7 @@ public class ValidatorTest
             [Test]
             public void should_have_one_failure_when_predicate_fail()
             {
-                Assert.That(Validate("").Failures.Count(), Is.EqualTo(1));
+                Assert.That(Validate("").Failures, Has.Count.EqualTo(1));
             }
 
             [Test]
@@ -108,19 +93,19 @@ public class ValidatorTest
                 [Test]
                 public void should_not_have_any_failures_when_condition_fail()
                 {
-                    Assert.That(Validate(default(TestModel)).Failures.Any(), Is.False);
+                    Assert.That(Validate(default(TestModel)).Failures, Is.Empty);
                 }
 
                 [Test]
                 public void should_not_have_any_failures_when_condition_and_predicate_pass()
                 {
-                    Assert.That(Validate("asdf").Failures.Any(), Is.False);
+                    Assert.That(Validate("asdf").Failures, Is.Empty);
                 }
 
                 [Test]
                 public void should_have_one_failure_when_condition_pass_and_predicate_fail()
                 {
-                    Assert.That(Validate("").Failures.Count(), Is.EqualTo(1));
+                    Assert.That(Validate("").Failures, Has.Count.EqualTo(1));
                 }
 
                 [Test]
@@ -143,19 +128,19 @@ public class ValidatorTest
                 [Test]
                 public void should_not_have_any_failures_when_condition_fail()
                 {
-                    Assert.That(Validate(default(TestModel)).Failures.Any(), Is.False);
+                    Assert.That(Validate(default(TestModel)).Failures, Is.Empty);
                 }
 
                 [Test]
                 public void should_not_have_any_failures_when_condition_and_predicate_pass()
                 {
-                    Assert.That(Validate("asdf").Failures.Any(), Is.False);
+                    Assert.That(Validate("asdf").Failures, Is.Empty);
                 }
 
                 [Test]
                 public void should_have_one_failure_when_condition_pass_and_predicate_fail()
                 {
-                    Assert.That(Validate("").Failures.Count(), Is.EqualTo(1));
+                    Assert.That(Validate("").Failures, Has.Count.EqualTo(1));
                 }
 
                 [Test]
@@ -188,7 +173,7 @@ public class ValidatorTest
             [Test]
             public void should_not_have_any_failures_when_predicates_pass()
             {
-                Assert.That(Validate("c").Failures.Any(), Is.False);
+                Assert.That(Validate("c").Failures, Is.Empty);
             }
 
             [Test]
@@ -231,7 +216,7 @@ public class ValidatorTest
             [Test]
             public void should_not_have_any_failures_when_predicates_pass()
             {
-                Assert.That(Validate("c").Failures.Any(), Is.False);
+                Assert.That(Validate("c").Failures, Is.Empty);
             }
 
             [Test]
@@ -275,6 +260,45 @@ public class ValidatorTest
         {
             public class before : Common<before.TestValidator>
             {
+                [Test]
+                public void should_not_have_any_failures_when_condition_fail()
+                {
+                    Assert.That(Validate(default(TestModel)).Failures.Any(), Is.False);
+                }
+
+                public class when_condition_pass : before
+                {
+                    [Test]
+                    public void should_not_have_any_failures_when_predicates_pass()
+                    {
+                        Assert.That(Validate("c").Failures, Is.Empty);
+                    }
+
+                    [Test]
+                    public void should_only_have_failure_for_first_predicate_when_both_fail()
+                    {
+                        Assert.That(Validate("abc").Failures.Single().Message == MessageHelper.NotEqualsMessage("abc"));
+                    }
+
+                    [Test]
+                    public void should_have_failure_for_second_predicate_when_first_pass_and_second_fail()
+                    {
+                        Assert.That(Validate("cba").Failures.Single().Message == null);
+                    }
+
+                    [Test]
+                    public void should_have_failure_for_property_when_first_predicate_fail()
+                    {
+                        Assert.That(Validate("abc").ShouldHaveFailureForProperty(nameof(TestModel.Value)));
+                    }
+
+                    [Test]
+                    public void should_have_failure_for_property_when_first_predicate_pass_and_second_fail()
+                    {
+                        Assert.That(Validate("cba").ShouldHaveFailureForProperty(nameof(TestModel.Value)));
+                    }
+                }
+
                 public class TestValidator : Validator<TestModel>
                 {
                     public TestValidator()
@@ -289,6 +313,45 @@ public class ValidatorTest
 
             public class in_middle : Common<in_middle.TestValidator>
             {
+                [Test]
+                public void should_not_have_any_failures_when_condition_fail_and_second_predicate_fail()
+                {
+                    Assert.That(Validate("cbab").Failures, Is.Empty);
+                }
+
+                public class when_condition_pass : in_middle
+                {
+                    [Test]
+                    public void should_not_have_any_failures_when_predicates_pass()
+                    {
+                        Assert.That(Validate("c").Failures, Is.Empty);
+                    }
+
+                    [Test]
+                    public void should_only_have_failure_for_first_predicate_when_both_fail()
+                    {
+                        Assert.That(Validate("abc").Failures.Single().Message == MessageHelper.NotEqualsMessage("abc"));
+                    }
+
+                    [Test]
+                    public void should_have_failure_for_second_predicate_when_first_pass_and_second_fail()
+                    {
+                        Assert.That(Validate("cba").Failures.Single().Message == null);
+                    }
+
+                    [Test]
+                    public void should_have_failure_for_property_when_first_predicate_fail()
+                    {
+                        Assert.That(Validate("abc").ShouldHaveFailureForProperty(nameof(TestModel.Value)));
+                    }
+
+                    [Test]
+                    public void should_have_failure_for_property_when_first_predicate_pass_and_second_fail()
+                    {
+                        Assert.That(Validate("cba").ShouldHaveFailureForProperty(nameof(TestModel.Value)));
+                    }
+                }
+
                 public class TestValidator : Validator<TestModel>
                 {
                     public TestValidator()
@@ -299,6 +362,136 @@ public class ValidatorTest
                             .Custom(value => !value.Contains("b"));
                     }
                 }
+            }
+        }
+    }
+
+    public class with_multiple_non_chained_predicates : Common<with_multiple_non_chained_predicates.TestValidator>
+    {
+        [Test]
+        public void should_not_have_any_failures_when_both_predicates_pass()
+        {
+            Assert.That(Validate("").Failures, Is.Empty);
+        }
+
+        [Test]
+        public void should_have_two_failures_when_both_predicates_fail()
+        {
+            Assert.That(Validate("ab").Failures, Has.Count.EqualTo(2));
+        }
+
+        [Test]
+        public void should_have_one_failure_when_first_predicate_pass_and_second_fail()
+        {
+            Assert.That(Validate("b").Failures, Has.Count.EqualTo(1));
+        }
+
+        [Test]
+        public void should_have_one_failure_when_first_predicate_fail_and_second_pass()
+        {
+            Assert.That(Validate("a").Failures, Has.Count.EqualTo(1));
+        }
+
+        [Test]
+        public void should_have_failure_for_property_when_first_predicate_fail_and_second_pass()
+        {
+            Assert.That(Validate("a").ShouldHaveFailureForProperty(nameof(TestModel.Value)));
+        }
+
+        [Test]
+        public void should_have_failure_for_property_when_first_predicate_pass_and_second_fail()
+        {
+            Assert.That(Validate("b").ShouldHaveFailureForProperty(nameof(TestModel.Value)));
+        }
+
+        public class TestValidator : Validator<TestModel>
+        {
+            public TestValidator()
+            {
+                For(model => model.Value).Custom(value => !value.Contains('a'));
+                For(model => model.Value).Custom(value => !value.Contains('b'));
+            }
+        }
+    }
+
+    public class with_nested_validator
+    {
+        public class with_no_condition : Common<with_no_condition.TestValidator>
+        {
+            [Test]
+            public void should_not_have_any_failures_when_nested_validator_pass()
+            {
+                Assert.That(Validate(new TestModel { NestedModel = new TestModel { Value = "a" } }).Failures, Is.Empty);
+            }
+
+            [Test]
+            public void should_have_one_failure_when_nested_validator_fail()
+            {
+                Assert.That(Validate(new TestModel { NestedModel = new TestModel { Value = "" } }).Failures, Has.Count.EqualTo(1));
+            }
+
+            [Test]
+            public void should_have_failure_for_property_when_nested_validator_fail()
+            {
+                Assert.That(Validate(new TestModel { NestedModel = new TestModel { Value = "" } })
+                    .ShouldHaveFailureForProperty(nameof(TestModel.Value)));
+            }
+
+            public class TestValidator : Validator<TestModel>
+            {
+                public TestValidator()
+                {
+                    For(model => model.NestedModel).Set(new NestedValidator());
+                }
+            }
+        }
+
+        public class with_condition : Common<with_condition.TestValidator>
+        {
+            [Test]
+            public void should_not_have_any_failures_when_condition_fail()
+            {
+                Assert.That(Validate(default(TestModel)).Failures, Is.Empty);
+            }
+
+            public class when_condition_pass : with_condition
+            {
+                [Test]
+                public void should_not_have_any_failures_when_nested_validator_pass()
+                {
+                    Assert.That(Validate(new TestModel { NestedModel = new TestModel { Value = "a" } }).Failures, Is.Empty);
+                }
+
+                [Test]
+                public void should_have_one_failure_when_nested_validator_fail()
+                {
+                    Assert.That(Validate(new TestModel { NestedModel = new TestModel { Value = "" } }).Failures, Has.Count.EqualTo(1));
+                }
+
+                [Test]
+                public void should_have_failure_for_property_when_nested_validator_fail()
+                {
+                    Assert.That(Validate(new TestModel { NestedModel = new TestModel { Value = "" } })
+                        .ShouldHaveFailureForProperty(nameof(TestModel.Value)));
+                }
+            }
+
+            public class TestValidator : Validator<TestModel>
+            {
+                public TestValidator()
+                {
+                    For(model => model.NestedModel)
+                        .If(model => model is not null)
+                        .Set(new NestedValidator());
+                }
+            }
+        }
+
+        public class NestedValidator : Validator<TestModel>
+        {
+            public NestedValidator()
+            {
+                For(model => model.Value).NotEmpty();
             }
         }
     }
@@ -315,12 +508,7 @@ public static class ValidatorTestExtensions
 public class TestModel
 {
     public string Value { get; set; }
-    public TestSubModel NestedModel { get; set; }
-}
-
-public class TestSubModel
-{
-    public string Value { get; set; }
+    public TestModel NestedModel { get; set; }
 }
 
 
